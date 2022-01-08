fc0371e157a91f312dd36f04c980bcaaa7b090c1
Matthew Broatch <mnbroatch@gmail.com>
2022-01-07 21:10:24 -0800

    build

--- dist/yarn-bound.js
+++ dist/yarn-bound.js
@@ -4,9 +4,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["yarn-bound"] = factory();
+		exports["YarnBound"] = factory();
 	else
-		root["yarn-bound"] = factory();
+		root["YarnBound"] = factory();
 })(this, function() {
 return /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
